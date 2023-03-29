784ff968af468095dd0294a1788f2dd12866ded8
Raunak Neupane <rezydev@gmail.com>
2023-03-29 17:51:22 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-def binary_search(a_list, an_item):
-   first = 0
-   last = len(a_list) - 1
-
-   while first <= last:
-       mid_point = (first + last) // 2
-       if a_list[mid_point] == an_item:
-           return True
-       else:
-           if an_item < a_list[mid_point]:
-               last = mid_point - 1
-           else:
-               first = mid_point + 1
-   return False
-
-
 def binary_search_rec(a_list, first, last, an_item):
    if len(a_list) == 0:
        return False
@@ -28,6 +12,21 @@ def binary_search_rec(a_list, first, last, an_item):
            else:
                first = mid_point + 1
                return binary_search_rec(a_list, first, last, an_item)
+            
+def binary_search(a_list, an_item):
+   first = 0
+   last = len(a_list) - 1
+
+   while first <= last:
+       mid_point = (first + last) // 2
+       if a_list[mid_point] == an_item:
+           return True
+       else:
+           if an_item < a_list[mid_point]:
+               last = mid_point - 1
+           else:
+               first = mid_point + 1
+   return False
 
 
 if __name__ == '__main__':
